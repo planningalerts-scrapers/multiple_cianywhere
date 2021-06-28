6353f099495e6a6365bb339a6e0060c0c4b61ee5
Matthew Landauer <mlandauer@gmail.com>
2021-06-28 15:44:40 +1000

    Handle "infinite" scrolling

--- scraper.rb
+++ scraper.rb
@@ -9,9 +9,11 @@ require "active_support/core_ext/time"
 
 Time.zone = "Sydney"
 
-# Because some systems are just TOO slow
-Capybara.default_max_wait_time = 10
+# Because some systems are just WAY TOO slow
+Capybara.default_max_wait_time = 60
 
+# Use :selenium_chrome instead below to watch what's going on which can
+# be very useful for debugging
 capybara = Capybara::Session.new(:selenium_chrome_headless)
 
 # This is the only url we can link to. It's the one for "Guest" access. Sigh.
@@ -22,20 +24,43 @@ capybara.visit(start_url)
 
 capybara.find("#Ci2Function3 a").click
 
-capybara.all(".thumbnailItem").each do |application|
-  record = {
-    "council_reference" => application.find(".headingField").text,
-    "address" => application.find(".subHeadingField").text,
-    "description" => application.find(".thbFld_Description").text,
-    # Great. Once again no direct links to individual applications on the open web
-    "info_url" => start_url,
-    # Might as well provide the date_scraped in the same timezone as date_received
-    "date_scraped" => Time.zone.now.to_s,
-    # Interprets the date in Sydney timezone
-    "date_received" => Time.zone.strptime(application.find(".thbFld_LodgedDate .editorField").text, "%d-%b-%Y %H:%M:%S").to_s
-  }
+# TODO: Select all types of development applications
 
-  pp record
+# First we want to find the total number of applications we expect to find
+# with the "infinite" scrolling
+
+total = capybara.find(".resultsRange").text.delete(",").to_i
+
+puts "Expecting #{total} applications in total"
+
+# TODO: What do we do here if we're not returning any results?
+
+applications = []
+
+loop do
+  count = applications.count
+  applications = capybara.all(".thumbnailItem", minimum: count + 1)
+  puts "Scrolled to #{applications.count}..."
+
+  capybara.find(".thumbnailViewContainer").scroll_to(:bottom)
+
+  applications[count+1..-1].each do |application|
+    record = {
+      "council_reference" => application.find(".headingField").text,
+      "address" => application.find(".subHeadingField").text,
+      "description" => application.find(".thbFld_Description").text,
+      # Great. Once again no direct links to individual applications on the open web
+      "info_url" => start_url,
+      # Might as well provide the date_scraped in the same timezone as date_received
+      "date_scraped" => Time.zone.now.to_s,
+      # Interprets the date in Sydney timezone
+      "date_received" => Time.zone.strptime(application.find(".thbFld_LodgedDate .editorField").text, "%d-%b-%Y %H:%M:%S").to_s
+    }
+
+    pp record
+  end
+
+  break if applications.count >= total
 end
 
 # capybara.save_and_open_page
