a0641e2bf4918b8e631cf2c0c5abbe302067a746
Matthew Landauer <mlandauer@gmail.com>
2021-06-28 12:14:42 +1000

    Include useless info_url

--- scraper.rb
+++ scraper.rb
@@ -14,8 +14,11 @@ Capybara.default_max_wait_time = 10
 
 capybara = Capybara::Session.new(:selenium_chrome_headless)
 
+# This is the only url we can link to. It's the one for "Guest" access. Sigh.
+start_url = "https://innerwest.t1cloud.com/T1Default/CiAnywhere/Web/INNERWEST/Public/LogOn/PRONLINESERVICE"
+
 # "Enter as a guest" button on inner west council website
-capybara.visit("https://innerwest.t1cloud.com/T1Default/CiAnywhere/Web/INNERWEST/Public/LogOn/PRONLINESERVICE")
+capybara.visit(start_url)
 
 capybara.find("#Ci2Function3 a").click
 
@@ -26,7 +29,8 @@ record = {
   "council_reference" => application.find(".headingField").text,
   "address" => application.find(".subHeadingField").text,
   "description" => application.find(".thbFld_Description").text,
-  # "info_url" => "",
+  # Great. Once again no direct links to individual applications on the open web
+  "info_url" => start_url,
   "date_scraped" => Date.today.to_s,
   # Interprets the date in Sydney timezone
   "date_received" => Time.zone.strptime(application.find(".thbFld_LodgedDate .editorField").text, "%d-%b-%Y %H:%M:%S").to_s
