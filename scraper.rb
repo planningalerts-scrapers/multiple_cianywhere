78db8cba77ca645a3548bb9fca8ea9f8e5d51b00
Matthew Landauer <mlandauer@gmail.com>
2022-06-22 12:36:50 +1000

    Use the words to find the right tile to click
    This is because in different authorities the id of the tile changes

--- scraper.rb
+++ scraper.rb
@@ -26,7 +26,7 @@ start_url = "https://innerwest.t1cloud.com/T1Default/CiAnywhere/Web/INNERWEST/Pu
 # "Enter as a guest" button on inner west council website
 capybara.visit(start_url)
 
-capybara.find("#Ci2Function1 a").click
+capybara.find(".tile", text: "Application Tracking").find("a").click
 
 # Filter for all types of development applications
 puts "Configuring filter..."
